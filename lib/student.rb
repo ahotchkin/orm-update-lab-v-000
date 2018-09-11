2744134d4f67e1b4146c5dc601495aeb24d3a88a
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-11 22:14:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,8 +46,10 @@ class Student
     end
   end
 
-  def self.create(row)
-
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def update
